0a5b616aa1722215017619469e4346005b1652ea
Brentan Rath <brentanrath@gmail.com>
2026-02-19 12:32:03 -0500

    Had supabase config in github env's as strings, not vars

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'process.env.SUPABASE_URL',
-  anonKey: 'process.env.SUPABASE_ANONKEY'
+  url: process.env.SUPABASE_URL,
+  anonKey: process.env.SUPABASE_ANONKEY
 };
